8a14e8283148cd11a94f6c394fe0326ae23d4c9d
Maria Felix <marialucienefelix@gmail.com>
2014-09-01 02:56:08 -0300

    Connection with database

--- tutoit/grails-app/conf/DataSource.groovy
+++ tutoit/grails-app/conf/DataSource.groovy
@@ -1,8 +1,8 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "root"
+    password = "root"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -13,20 +13,29 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/tutoit?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "root"
+
+           }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
+             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/tutoit?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "root"
+
+          }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/tutoit?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "root"
+
             pooled = true
             properties {
                maxActive = -1
