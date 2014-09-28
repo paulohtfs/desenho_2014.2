b435e556e026212b48898039dca67d590a997f8a
tiagorc <tiagobrasil2005@yahoo.com.br>
2014-09-27 21:48:22 -0300

    Finishing the configuration of Spring Security

---
+++ tutoit/grails-app/controllers/tutoit/SensitiveContentController.groovy
@@ -0,0 +1,11 @@
+package tutoit
+
+import grails.plugins.springsecurity.Secured
+
+class SensitiveContentController {
+
+	@Secured(['ROLE_ADMIN'])
+    def index() {
+    	render "Some sensitive content"
+    }
+}
