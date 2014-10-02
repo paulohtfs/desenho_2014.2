021285b6549f6629811edf55cf327fb6951b8a96
lu <luciene@lu.(none)>
2014-10-02 18:40:31 -0300

    Fixing org.compass-project#compass;2.2.1 dependence

--- tutoit/grails-app/conf/BuildConfig.groovy
+++ tutoit/grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
         // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
@@ -36,6 +36,8 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://repo.grails.org/grails/core"
+        mavenRepo "https://oss.sonatype.org/content/repositories/releases"
     }
 
     dependencies {
