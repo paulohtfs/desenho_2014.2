9d39323e9d774e467c0ded7ac6542b006f0b2984
paulohtfs <paulohtfs@gmail.com>
2014-11-10 18:24:17 -0200

    Fixing bootstrap exeption

--- tutoit/grails-app/conf/BootStrap.groovy
+++ tutoit/grails-app/conf/BootStrap.groovy
@@ -1,20 +1,6 @@
-import tutoit.User
-import tutoit.Login
-import tutoit.UserLogin
 
 class BootStrap {
     def init = { servletContext ->
-        def userRole = Login.findByAuthority('ROLE_USER') ?: new Login(authority: 'ROLE_USER').save(failOnError: true)
-        def adminRole = Login.findByAuthority('ROLE_ADMIN') ?: new Login(authority: 'ROLE_ADMIN').save(failOnError: true)
-
-        def adminUser = User.findByUsername('admin') ?: new User(
-                username: 'admin',
-                password: springSecurityService.encodePassword('admin'),
-                enabled: true).save(failOnError: true)
-
-        if (!adminUser.authorities.contains(adminRole)) {
-            UserLogin.create adminUser, adminRole
-        }
     }
 
     def destroy = {
